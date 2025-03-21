8ae69e4c3ea5bafc1d069aa374ec75d58869db1b
PracticaFC <pprogra2023@gmail.com>
2025-03-20 20:55:06 -0600

    Update db.php

--- db.php
+++ db.php
@@ -2,11 +2,11 @@
 $host = 'localhost';
 $user = 'root';
 $password = '';
-$dbname = 'crud_db'; // Asegúrate de que el nombre es correcto
+$dbname = 'crud_db'; 
 
 $conn = new mysqli($host, $user, $password, $dbname);
 
-// Verifica si la conexión fue exitosa
+
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
